3ae067d8fd9f3a8f1a7129d6a9db1cc22358cf78
JYMoon99 <munj5323@gmail.com>
2023-10-23 23:53:26 +0900

    (2023. 10. 23)

--- Platform2D/Assets/Script/Player.cs
+++ Platform2D/Assets/Script/Player.cs
@@ -6,17 +6,22 @@ public class Player : MonoBehaviour
 {
     [SerializeField] float speed;
     [SerializeField] float jumpPower;
+    [SerializeField] float maxSpeed;
+    [SerializeField] float health;
+
 
     Rigidbody2D rigid;
     Animator anim;
     SpriteRenderer render;
 
+    public bool isJump;
 
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
         render = GetComponent<SpriteRenderer>();
+
     }
 
     void FixedUpdate()
@@ -28,8 +33,6 @@ public class Player : MonoBehaviour
     void Update()
     {
         Jump();
-
-        
     }
 
 
@@ -37,10 +40,9 @@ public class Player : MonoBehaviour
     {
         float h = Input.GetAxisRaw("Horizontal");
 
-        rigid.AddForce(new Vector2(h * speed * Time.deltaTime, 0), ForceMode2D.Impulse);
+        rigid.AddForce(Vector2.right * h * speed * Time.deltaTime, ForceMode2D.Impulse);
 
-        if (rigid.velocity.x > 20)
-            rigid.velocity = new Vector2(20, rigid.velocity.y);
+
 
 
         if (Input.GetAxisRaw("Horizontal") < 0)
@@ -53,20 +55,47 @@ public class Player : MonoBehaviour
             anim.SetBool("isRun", true);
         else
             anim.SetBool("isRun", false);
+            
+
+
 
     }
 
     void Jump()
     {
-        if(Input.GetButtonDown("Jump"))
+        if(Input.GetButtonDown("Jump") && !isJump)
         {
             rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
-
+            isJump = true;
             anim.SetBool("isJump", true);
         }
         else
         {
             anim.SetBool("isJump", false);
         }
+
+        RaycastHit2D rayCast;
+
+        rayCast = Physics2D.Raycast(transform.position, Vector2.down * 0.4f, 0.5f, LayerMask.GetMask("Plat"));
+
+        Debug.DrawRay(transform.position, Vector2.down * 1f, Color.green);
+
+        if (rayCast)
+        {
+            isJump = false;
+        }
+
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+       if(collision.gameObject.tag == "Enemy")
+        {
+            health -= 30;
+        }
+
+
+
+    }
+
 }
